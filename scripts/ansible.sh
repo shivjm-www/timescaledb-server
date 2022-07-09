6cee0dcf7d68b268c3b4190802bfdd155a6487e6
Shiv Jha-Mathur <shiv@shivjm.in>
2022-07-09 16:53:00 +0530

    fix(scripts): use apt-get instead of apt

--- scripts/ansible.sh
+++ scripts/ansible.sh
@@ -1,9 +1,9 @@
 #!/bin/bash -eux
-# https://github.com/geerlingguy/packer-boxes/blob/55412993a04d3d81ee0a61559cfd993e6d0907ad/debian11/scripts/ansible.sh
+# adapted from https://github.com/geerlingguy/packer-boxes/blob/55412993a04d3d81ee0a61559cfd993e6d0907ad/debian11/scripts/ansible.sh
 
 # Install Ansible dependencies.
-apt -y update && apt-get -y upgrade
-apt -y install python3-pip python3-dev
+apt-get -y update && apt-get -y upgrade
+apt-get -y install python3-pip python3-dev
 
 # Install Ansible.
 pip3 install ansible
